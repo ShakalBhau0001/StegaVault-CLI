7482792fba26ff0871ddc89da9b1cab891d33009
Shakalen Shaikh <160219957+ShakalBhau0001@users.noreply.github.com>
2026-01-19 00:18:45 +0530

    Update file_cli.py

--- cli/file_cli.py
+++ cli/file_cli.py
@@ -1,1 +1,11 @@
+from core.file_crypto import encrypt_file, decrypt_file
 
+
+def handle_file(args):
+    if args.encrypt:
+        encrypt_file(args.input, args.password)
+        print("[+] File encrypted successfully")
+
+    elif args.decrypt:
+        decrypt_file(args.input, args.password)
+        print("[+] File decrypted successfully")
